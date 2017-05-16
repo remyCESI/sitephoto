e3f8e5215cabe1b13e33fa3d1d6a25f0edc4abe9
remyCESI <remy.vancamp@viacesi.fr>
2017-05-16 16:39:24 +0200

    Add files via upload

---
+++ test.php
@@ -0,0 +1,228 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <meta name="Remy vancamp" content="">
+
+
+    <title>Cover Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="bootstrap-3.3.7/docs/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="bootstrap-3.3.7/docs/examples/cover/cover.css" rel="stylesheet">
+  <link href="font-awesome-4.7.0/css/font-awesome.min.css " rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="bootstrap-3.3.7/docs/assets/js/ie-emulation-modes-warning.js"></script>
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+
+  <div class="site-wrapper" data-id="logo">
+      <div class="site-wrapper-inner" style="background-image: url('LOGO.jpg')">
+
+            <span>
+               <i class="fa fa-chevron-down fa-2x" aria-hidden="true" style="position: absolute; top: 650px; left:740px" type="submit"></i>
+             </span>
+      </div>
+  </div>
+
+
+<div class="site-wrapper" id="acceuil"  style="background-image: url('rail.jpg')">
+  <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">@Frenchtarget</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="#acceuil"><a href="#">Home</a></li>
+              <li><a href="#shooting">Shooting</a></li>
+              <li><a href="#tools">Tools</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </nav>
+
+
+
+
+
+
+          <div class="well" style="color:black; background-color: rgba(206, 206, 206, 0.6); font-family:'impact';" >
+
+            <h1 class="cover-heading" >PHOTOGRAPHIE.</h1>
+            <p class="lead" > STREETSHOOT, SHOOTING, LANDSCAPE,...</p>
+            <h3>
+            <span class="label label-default"></span>
+            </h3>
+          </div>
+
+
+
+
+      </div>
+
+    <section  id="shooting">
+          <h1 class="page-header" style="font-family: impact"> Shooting </h1>
+    <div class="container marketing">
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row">
+
+        <div class="col-lg-4">
+          <img class="img-rounded" src="winterLand.png" alt="Generic placeholder image" width="140" height="140">
+          <h2> Winter Landscape</h2>
+          <p></p>
+          <p><a class="btn btn-default" href="#" role="button"> Consulter </a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-rounded" src="smorgane.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Shooting Morgane</h2>
+          <p></p>
+          <p><a class="btn btn-default" href="#" role="button"> Consulter </a></p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="img-rounded" src="prochain.png" alt="Generic placeholder image" width="140" height="140">
+          <h2>Prochain</h2>
+          <p></p>
+          <p><a class="btn btn-default" href="#" role="button"> Consulter </a></p>
+        </div><!-- /.col-lg-4 -->
+      </section><!-- /.row -->
+
+
+    <section id="tools">
+
+        <h1 class="page-header" style="font-family: impact"> Equipement </h1>
+
+      <div class="row featurette">
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Pentax k-50.</h2>
+
+
+
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="400x400" src="k-50.png">
+        </div>
+      </div>
+      <hr class="featurette-divider">
+      <div class="row featurette">
+        <div class="col-md-7 col-md-push-5">
+          <h2 class="featurette-heading">Objectif </h2>
+
+        </div>
+        <div class="col-md-5 col-md-pull-7">
+          <img class="featurette-image img-responsive center-block" data-src="holder.js/500x500/auto" alt="400x400" src="objectif.png">
+        </div>
+      </div>
+    </section>
+
+
+<hr class="featurette-divider">
+  <div id="contact" >
+  <h1  style="font-family: impact"> Contact </h1>
+  <p>  </p>
+
+
+
+  <form method="post" action="" >
+<div class="input-group">
+  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+  <input id="nom" type="text" class="form-control" name="nom" placeholder="Name">
+</div>
+
+<div class="input-group">
+  <span class="input-group-addon"><i class="glyphicon glyphicon-flag"></i></span>
+  <input id="password" type="text" class="form-control" name="subject" placeholder="Subject">
+</div>
+
+
+<div class="input-group">
+  <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+  <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+</div>
+
+<div class="input-group">
+  <span class="input-group-addon"><i class="glyphicon glyphicon-comment"></i></span>
+  <input id="msg" type="text" class="form-control" name="msg" placeholder="Your message">
+</div>
+<p></p>
+<button class="btn btn-secondary" name="envoyer" style="color: black" type="submit">Envoyer </button>
+
+</form>
+<?php
+if(isset($_POST['envoyer']) && empty($_POST['email'])){
+  $to="remy.vancamp@viacesi.fr";
+
+  $subject=$_POST['subject'];
+  $message=$_POST['msg'];
+  /*$headers= 'from: $_post["email"]'."\n"
+  $headers.='content-type:text/html; charset="iso-8859-1"'."\n"
+  $headers.='content-transfert-encoding:8bit';*/
+
+  mail($to,$subject,$message);
+  echo "votre mail a bien été envoyé";
+}
+?>
+
+  </div>
+
+
+
+    <footer>
+    <div>
+    <ul class='nav pull-center footer2'>
+            <li>
+            <a class="btn btn-social-icon btn-instagram" href="https://www.instagram.com/frenchtarget/" >
+            <i class="fa fa-instagram"> Instagram</i>
+            </a>
+            </li>
+             <li>
+            <a class="btn btn-social-icon btn-envelope-o" href="mailto:remy.vancamp@viacesi.com">
+            <i class="fa fa-envelope-o"> Mail</i>
+            </a>
+            </li>
+            </ul>
+            </div>
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="bootstrap-3.3.7/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="bootstrap-3.3.7/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
+
+  </body>
+</html>
